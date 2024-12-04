55d560cf707d1827e0e3bb563be3951992e97913
omarcolakhodzic24 <omar.colakhodzic@edu.fit.ba>
2024-12-04 12:21:11 +0100

    ﻿Login jwt.

--- SyncPoint365.API/Controllers/AuthController.cs
+++ SyncPoint365.API/Controllers/AuthController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using SyncPoint365.API.RESTModels;
+using SyncPoint365.Core.DTOs.Users;
 using SyncPoint365.Service.Common.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SyncPoint365.API.Controllers
 {
@@ -9,10 +14,12 @@ namespace SyncPoint365.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IUsersService _usersService;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(IUsersService usersService)
+        public AuthController(IUsersService usersService, IConfiguration configuration)
         {
             _usersService = usersService;
+            _configuration = configuration;
         }
 
         [HttpPost]
@@ -22,12 +29,44 @@ namespace SyncPoint365.API.Controllers
             try
             {
                 var user = await _usersService.ValidateUserAsync(model.Email, model.Password, cancellationToken);
-                return Ok(new { Message = "User authenticated successfully", User = user });
+
+                var token = GenerateToken(user);
+
+                return Ok(new { Message = "User authenticated successfully", User = user, Token = token });
             }
             catch (UnauthorizedAccessException)
             {
                 return Unauthorized("Invalid email or password!");
             }
+        }
+
+        [HttpPost]
+        [Route("GenerateToken")]
+        public string GenerateToken(UserDTO user)
+        {
+            var claimsIdentity = new ClaimsIdentity(new[]
+            {
+               new Claim(ClaimTypes.Name, user.Email),
+               new Claim(ClaimTypes.NameIdentifier, user.Email),
+               new Claim(ClaimTypes.Role, user.Role.ToString())
+           });
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:SecretKey"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claimsIdentity,
+                Expires = DateTime.Now.AddMinutes(90),
+                Issuer = _configuration["JwtSetting:Issuer"],
+                Audience = _configuration["JwtSetting:Audience"],
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
--- SyncPoint365.Core/DTOs/Users/UserDTO.cs
+++ SyncPoint365.Core/DTOs/Users/UserDTO.cs
@@ -7,6 +7,7 @@ namespace SyncPoint365.Core.DTOs.Users
         public string FirstName { get; set; } = default!;
         public string LastName { get; set; } = default!;
         public string FullName { get; set; } = default!;
+        public string Email { get; set; } = default!;
         public Role Role { get; set; }
         public bool isActive { get; set; }
     }
