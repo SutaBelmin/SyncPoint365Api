14d1effad73e0daceaabbf961dab2cada6376b35
omarcolakhodzic24 <omar.colakhodzic@edu.fit.ba>
2025-01-30 10:30:00 +0100

    Removed configuration.

--- SyncPoint365.Service/Services/CompanyHolidaysService.cs
+++ SyncPoint365.Service/Services/CompanyHolidaysService.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using FluentValidation;
-using Microsoft.Extensions.Configuration;
 using SyncPoint365.Core.DTOs.CompanyHolidays;
 using SyncPoint365.Core.Entities;
 using SyncPoint365.Repository.Common.Interfaces;
@@ -12,14 +11,12 @@ namespace SyncPoint365.Service.Services
     {
         private readonly ICompanyHolidaysRepository _repository;
         protected readonly IMapper _mapper;
-        private readonly IConfiguration _configuration;
 
-        public CompanyHolidaysService(ICompanyHolidaysRepository repository, IMapper mapper, IValidator<CompanyHolidayAddDTO> addValidator, IValidator<CompanyHolidayUpdateDTO> updateValidator, IConfiguration configuration)
+        public CompanyHolidaysService(ICompanyHolidaysRepository repository, IMapper mapper, IValidator<CompanyHolidayAddDTO> addValidator, IValidator<CompanyHolidayUpdateDTO> updateValidator)
             : base(repository, mapper, addValidator, updateValidator)
         {
             _repository = repository;
             _mapper = mapper;
-            _configuration = configuration;
         }
     }
 }
