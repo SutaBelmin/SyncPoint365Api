57fd83bbf2b0fdac4cd6aaa23333cd0f3ce24cba
omarcolakhodzic24 <omar.colakhodzic@edu.fit.ba>
2025-01-30 08:29:37 +0100

    Added policy.

--- SyncPoint365.API/Controllers/CompanyNewsController.cs
+++ SyncPoint365.API/Controllers/CompanyNewsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SyncPoint365.API.Helpers;
 using SyncPoint365.Core.DTOs.CompanyNews;
 using SyncPoint365.Core.Helpers;
@@ -18,6 +19,7 @@ namespace SyncPoint365.API.Controllers
 
         [HttpGet]
         [Route("paged", Name = "SyncPoint365-GetCompanyNewsPagedListAsync")]
+        [Authorize(Policy = "AdminEmployeePolicy")]
         public async Task<IActionResult> GetCompanyNewsPagedListAsync(string? query = null, bool? isVisible = null, DateTime? dateFrom = null, DateTime? dateTo = null, string? orderBy = null,
             int page = Constants.Pagination.PageNumber, int pageSize = Constants.Pagination.PageSize, CancellationToken cancellationToken = default)
         {
@@ -32,6 +34,7 @@ namespace SyncPoint365.API.Controllers
 
         [HttpPut]
         [Route("Change-Visibility", Name = "SyncPoint365-ChangeVisibility")]
+        [Authorize(Policy = "SuperAdminPolicy")]
         public async Task<IActionResult> UpdateVisibility([FromQuery] int id, CancellationToken cancellationToken)
         {
             var result = await _companyNewsService.UpdateVisibilityAsync(id, cancellationToken);
@@ -45,6 +48,7 @@ namespace SyncPoint365.API.Controllers
 
         [HttpPost]
         [Route("add", Name = "SyncPoint365-AddCompanyNews")]
+        [Authorize(Policy = "SuperAdminPolicy")]
         public async Task<IActionResult> AddCompanyNewsAsync([FromBody] CompanyNewsAddDTO dto, CancellationToken cancellationToken)
         {
             try
